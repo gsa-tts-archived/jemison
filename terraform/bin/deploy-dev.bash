cd39054075b72368b2b915e2ffe4d03dcd81eebf
Matt Jadud <matthew.jadud@gsa.gov>
2025-01-05 10:56:03 -0500

    Root is needed in the runner
    Apparently.

--- terraform/bin/deploy-dev.bash
+++ terraform/bin/deploy-dev.bash
@@ -3,12 +3,12 @@
 # This script runs in the GH Actions environment.
 # Running and testing locally will not work.
 
-apt-get update
-apt-get install -y zip python
+sudo apt-get update
+sudo apt-get install -y zip python
 
 # Install CF CLI
 curl -k -O -L https://github.com/cloudfoundry/cli/releases/download/v8.8.0/cf8-cli-installer_8.8.0_x86-64.deb
-apt-get install --assume-yes ./cf8-cli-installer_8.8.0_x86-64.deb
+sudo apt-get install --assume-yes ./cf8-cli-installer_8.8.0_x86-64.deb
 
 # Authenticate against Cloud.gov
 cf api api.fr.cloud.gov
