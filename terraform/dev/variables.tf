f4983ead455312da10c700fb96d187bbf20dd4cd
Matt Jadud <matthew.jadud@gsa.gov>
2025-01-05 15:44:36 -0500

    Another approach...

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -26,14 +26,14 @@ variable "aws_default_region" {
   type      = string
   sensitive = true
 }
-variable "aws_secret_access_key" {
-  type      = string
-  sensitive = true
-}
-variable "aws_access_key_id" {
-  type      = string
-  sensitive = true
-}
+# variable "aws_secret_access_key" {
+#   type      = string
+#   sensitive = true
+# }
+# variable "aws_access_key_id" {
+#   type      = string
+#   sensitive = true
+# }
 variable "bucket_name" {
   type      = string
   sensitive = true
