fb78a1c5bb94292767e051f50abfe90c53d42de5
Luis Metzger <luis.metzger@inadev.com>
2025-02-04 11:10:40 -0700

    Remove log.

--- internal/queueing/generic_insert.go
+++ internal/queueing/generic_insert.go
@@ -100,8 +100,6 @@ func Enqueue(chQSHP <-chan QSHP) {
 			commonCommit(qshp, ctx, tx)
 
 		case "collect":
-			zap.L().Debug("----- I am in the collect enqueue -----")
-
 			_, err := client.InsertTx(ctx, tx, common.CollectArgs{
 				Scheme: qshp.Scheme,
 				Host:   qshp.Host,
